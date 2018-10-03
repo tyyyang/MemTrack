ddc564d6d7ca3bc22a353c23ed86736561fcc4bf
skyoung <tianyu.young@gmail.com>
2018-10-03 10:22:13 +0800

    fix demo

--- tracking/demo.py
+++ tracking/demo.py
@@ -3,71 +3,26 @@ import time
 import sys
 sys.path.append('../')
 import os
-import json
 import config
 from tracking.tracker import Tracker, Model
-from collections import OrderedDict
 import cv2
 
-class Sequence:
+def load_seq_config(seq_name):
 
-    def __init__(self, name, path, startFrame, endFrame, attributes,
-        nz, ext, imgFormat, gtRect, init_rect):
-        self.name = name
-        self.path = path
-        self.startFrame = startFrame
-        self.endFrame = endFrame
-        self.attributes = attributes
-        self.nz = nz
-        self.ext = ext
-        self.imgFormat = imgFormat
-        self.gtRect = gtRect
-        self.init_rect = init_rect
-        self.__dict__ = OrderedDict([
-            ('name', self.name),
-            ('path', self.path),
-            ('startFrame', self.startFrame),
-            ('endFrame', self.endFrame),
-            ('attributes', self.attributes),
-            ('nz', self.nz),
-            ('ext', self.ext),
-            ('imgFormat', self.imgFormat),
-            ('init_rect', self.init_rect),
-            ('gtRect', self.gtRect)])
+    src = os.path.join(config.otb_data_dir,seq_name,'groundtruth_rect.txt')
+    gt_file = open(src)
+    lines = gt_file.readlines()
+    gt_rects = []
+    for gt_rect in lines:
+        rect = [int(v) for v in gt_rect[:-1].split(',')]
+        gt_rects.append(rect)
 
-def load_seq_config(name):
-    if name == 'Jogging-1':
-        seq_name = 'Jogging'
-        config_name = 'cfg1.json'
-    elif name == 'Jogging-2':
-        seq_name = 'Jogging'
-        config_name = 'cfg2.json'
-    elif name == 'Skating2-1':
-        seq_name = 'Skating2'
-        config_name = 'cfg1.json'
-    elif name == 'Skating2-2':
-        seq_name = 'Skating2'
-        config_name = 'cfg2.json'
-    else:
-        seq_name = name
-        config_name = 'cfg.json'
-    src = os.path.join(config.otb_data_dir,seq_name,config_name)
-    configFile = open(src)
-    string = configFile.read()
-    j = json.loads(string)
-    seq = Sequence(**j)
-    seq.path = os.path.join(os.path.abspath(seq.path), '')
+    init_rect= gt_rects[0]
+    img_path = os.path.join(config.otb_data_dir,seq_name,'img')
+    img_names = sorted(os.listdir(img_path))
+    s_frames = [os.path.join(img_path, img_name) for img_name in img_names]
 
-    seq.len = seq.endFrame - seq.startFrame + 1
-    seq.s_frames = [None] * seq.len
-
-    for i in range(seq.len):
-        image_no = seq.startFrame + i
-        _id = seq.imgFormat.format(image_no)
-        seq.s_frames[i] = seq.path + _id
-
-    seq.init_rect = seq.gtRect[0]
-    return seq
+    return init_rect, s_frames
 
 def display_result(image, pred_boxes, frame_idx, seq_name=None):
     if len(image.shape) == 3:
@@ -90,16 +45,16 @@ def run_tracker():
         os.chdir('../')
         model = Model(sess)
         tracker = Tracker(model)
-        seq = load_seq_config('Basketball')
-        bbox = seq.init_rect
+        init_rect, s_frames = load_seq_config('Basketball')
+        bbox = init_rect
         res = []
         res.append(bbox)
         start_time = time.time()
-        tracker.initialize(seq.s_frames[0], bbox)
+        tracker.initialize(s_frames[0], bbox)
 
-        for idx in range(1, len(seq.s_frames)):
+        for idx in range(1, len(s_frames)):
             tracker.idx = idx
-            bbox, cur_frame = tracker.track(seq.s_frames[idx])
+            bbox, cur_frame = tracker.track(s_frames[idx])
             display_result(cur_frame, bbox, idx)
             res.append(bbox.tolist())
         end_time = time.time()
